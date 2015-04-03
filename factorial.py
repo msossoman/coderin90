4352da83d7c9a9908189a0487e56e1a78b42654c
Mark Sossoman <msossoman@gmail.com>
2015-04-03 19:12:34 -0400

    various changes and updates

--- factorial.py
+++ factorial.py
@@ -3,4 +3,16 @@ def factorial(n):
 		return n * factorial(n-1)
 	return 1
 
-print factorial(3)
+print factorial(4)
+
+'''def user():
+	n = (raw_input("What number would you like to use?"))
+	try:
+		factorial(int(n))
+	except ValueError:
+		print "You must choose a number. Please try again."
+		return user()
+
+
+if __name__ == '__main__':
+	user()'''
